fd081be9cd5686c7120d72c28bbd6baf51d2e915
blazingcody0707 <114349633+blazingcody0707@users.noreply.github.com>
2026-06-25 16:03:28 -0400

    Include 'amd64' in ARCHITECTURES list
    Added 'amd64' architecture to the ARCHITECTURES list.

--- gen_data/gen-suite-matrix.py
+++ gen_data/gen-suite-matrix.py
@@ -8,7 +8,7 @@ SUITES_DIR="./suites"
 # it has high priority over the architecture specified in the suite
 # set it to "all" to build all architectures defined in the <suite>/<varient>.sh file
 # "all" is currently broken!
-ARCHITECTURES = [ "arm64", "armhf" ]
+ARCHITECTURES = [ "arm64", "armhf" "amd64" ]
 
 # Can be moved to utils?
 def generate_matrix_json() -> str:
